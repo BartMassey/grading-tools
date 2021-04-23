52852740126731ba24825ab86aa675cca84ddf03
Bart Massey <bart.massey@gmail.com>
2021-04-23 16:01:37 -0700

    fixed grade-rust.py to use JSON test output

--- grade-rust.py
+++ grade-rust.py
@@ -1,20 +1,40 @@
 #!/usr/bin/python3
 # Do basic Rust HW tests
 
-import argparse
+import argparse, json, shutil, subprocess
 from pathlib import Path
-import shutil, subprocess
 
 ap = argparse.ArgumentParser("Grade a Rust assignment.")
 ap.add_argument(
     "-t", "--tests",
-    help = "file of tests to run with student's assignment"
+    help = "file of tests to run with student's assignment",
 )
 args = ap.parse_args()
 
-def round(name, cmd):
+def run_tests(cmd):
+    result = subprocess.run(
+        cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.DEVNULL,
+        encoding="utf-8",
+    )
+    if result.returncode != 0:
+        for line in result.stdout.split("\n"):
+            if line.strip() == "":
+                continue
+            record = json.loads(line)
+            if record["type"] == "test" and record["event"] == "failed":
+                print(f'test failed: {record["name"]}')
+                print(record["stdout"], end="")
+    return result
+
+def round(name, cmd, filtered=None):
     print(f"* checking {name}")
-    result = subprocess.run(cmd.split())
+    cmd = cmd.split()
+    if filtered is None:
+        result = subprocess.run(cmd)
+    else:
+        result = filtered(cmd)
     if result.returncode != 0:
         print(f"* {name} check failed")
 
@@ -33,5 +53,9 @@ if args.tests:
         print(f"{test_target}: exists, not overwriting")
     else:
         shutil.copy(auxtests, test_target)
-round("cargo test", "cargo test -- -q")
+round(
+    "cargo test",
+    "cargo test -- -Zunstable-options --format=json --report-time",
+    filtered=run_tests,
+)
 clean()
