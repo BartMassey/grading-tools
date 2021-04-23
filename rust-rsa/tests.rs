b2965bbd6d257bf979a3739bbe38a2c1ee85020a
Bart Massey <bart.massey@gmail.com>
2021-04-23 15:18:18 -0700

    added rust-rsa hw tests

---
+++ rust-rsa/tests.rs
@@ -0,0 +1,31 @@
+#![allow(non_snake_case)]
+
+use rand::Rng;
+
+use toy_rsa::*;
+
+#[test]
+fn test_exp() {
+    assert_eq!(EXP, 65_537);
+}
+
+#[test]
+fn test_rsa_example() {
+    let msg = 12345;
+    let (p, q) = (0xed23e6cd, 0xf050a04d);
+    let cmsg = encrypt(p as u64 * q as u64, msg);
+    let pmsg = decrypt((p, q), cmsg);
+    assert_eq!(msg, pmsg);
+}
+
+#[test]
+fn test_rsa_random() {
+    let mut rng = rand::thread_rng();
+    for _ in 0..100 {
+        let plain = rng.gen_range(0..u32::max_value());
+        let (p, q) = toy_rsa::genkey();
+        let cipher = toy_rsa::encrypt(p as u64 * q as u64, plain);
+        let decrypted = toy_rsa::decrypt((p, q), cipher);
+        assert_eq!(plain, decrypted);
+    }
+}
